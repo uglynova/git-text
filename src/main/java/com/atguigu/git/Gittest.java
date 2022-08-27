f31046fac22a0e910f34d3c032461a7f55e7d458
Xu <Xu@sacjan.com>
2022-08-27 14:51:48 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/Gittest.java
+++ src/main/java/com/atguigu/git/Gittest.java
@@ -5,5 +5,6 @@ public class Gittest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hot-fix git");
     }
 }
